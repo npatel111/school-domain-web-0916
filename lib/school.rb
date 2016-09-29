5a73effe341a13a256152c31f3ca4176c7d0a83d
Niti Nrw <niti.patel@flatironschool.com>
2016-09-29 18:56:52 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+require 'pry'
+
+class School
+
+  attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+  #binding.pry
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sorted = {}
+    roster.each do |grade, students|
+      sorted[grade] = students.sort
+    end
+    sorted
+  end
+
+end
